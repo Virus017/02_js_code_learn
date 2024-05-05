ae77195ea7d505c3e8d14efcf028d522a45e00fc
Virus017 <chargingreth@gmail.com>
2024-05-05 16:40:23 +0530

    arrays

---
+++ 01_arrays.js
@@ -0,0 +1,34 @@
+// Arrays
+
+const array = [0, 1, 2, 3]
+const newArray = ["shaktiman", "nagraj"]
+
+const Arr1 = new Array(3, 5, 53, 5)
+// console.log(array[3])
+
+// Methods of Array 
+
+// array.push(4)
+// array.push(7)
+// array.pop()
+
+// array.unshift(8) //add element in the begining
+// array.shift()    //remove element in the begining
+
+// console.log(array.includes(2))
+
+// const arr2 = array.join()   //change it to string
+// console.log(array)
+// console.log(arr2)
+
+//Slice & splice
+
+console.log("A", array)
+
+const arr3 =array.slice(0,2)
+console.log(arr3)
+console.log("B", array)
+
+const arr4 = array.splice(0, 2)
+console.log(arr4)
+console.log("C", array)
