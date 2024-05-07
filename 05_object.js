f8cef36a7bfdbef41d646f568664a8f2f351c8bd
Virus017 <chargingreth@gmail.com>
2024-05-07 21:48:53 +0530

    objects and jason

---
+++ 05_object.js
@@ -0,0 +1,26 @@
+const course = {
+    name: "learn js",
+    fees: 2000,
+    instructor: "devv"
+}
+
+// console.log(course.instructor)
+
+const {instructor: inst} = course;    //this is called distructuring
+
+console.log(inst);
+
+// API's
+
+// {
+//     "name": "learn js",
+//     "fees": 2000,
+//     "instructor": "devv"
+
+// }
+
+[
+    {},
+    {},
+    {},
+]
