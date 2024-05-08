0194bd67b974d3b6bbc695f7991cc64faa0d2859
Virus017 <chargingreth@gmail.com>
2024-05-08 23:24:02 +0530

    fuction with object and arrays

--- fuctions/01_fuctions.js
+++ fuctions/01_fuctions.js
@@ -30,4 +30,35 @@ function logginMessage (username) {
     return `${username}, just logged in`
 }
 
-console.log(logginMessage())
+// console.log(logginMessage())
+
+// function priceCalculate (...num1) {    // used for multiple value
+//     return num1
+// }
+
+// console.log(priceCalculate(23, 53, 3, 2))
+
+function priceCalculate (val1, val2, ...num1) {    
+    return num1
+}
+
+console.log(priceCalculate(23, 53, 3, 2))  // herre 3 & 2 goes to num1
+
+const user = {
+    product: "bag",
+    price: 299
+}
+
+function objectData(randomObject) {
+    console.log(`Purchsed is ${randomObject.product} and price is ${randomObject.price}`)
+}
+
+objectData(user)
+
+const newArray = [200, 300, 500]
+
+function getArray(item) {
+    return item[1]
+}
+
+console.log(getArray(newArray))
