1646d46ec8d7ff9c6e7bedb6a47a544a0582d451
Virus017 <chargingreth@gmail.com>
2024-05-13 14:45:04 +0530

    arrow

--- fuctions/03_arrow.js
+++ fuctions/03_arrow.js
@@ -36,7 +36,8 @@ const chai = () => {        //arrow function
 //     return num1 + num2
 // }
 // const add = (num1, num2) =>  num1 + num2
-const add = (num1, num2) =>  (num1 + num2)
+// const add = (num1, num2) =>  (num1 + num2)
+const add = (num1, num2) =>  num1 + num2
 
 
 console.log(add(2,4));
